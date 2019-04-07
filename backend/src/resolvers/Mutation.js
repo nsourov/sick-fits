83c7d597b11b803b11a534aca860bcb319976b4c
Naimur Rahman <naimur.rahman103@gmail.com>
2019-04-07 12:43:02 +0600

    15 no video

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,9 +1,13 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    const newDog = args;
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
+    return item;
   }
 };
 
